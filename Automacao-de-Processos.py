63e2a2a64aa45985bfe296a2ff48d2eb97fcb496
jdeveloperanalyst <jonatas1200@icloud.com>
2022-11-11 17:49:46 -0300

    Pegando a cotação do dólar

---
+++ Automacao-de-Processos.py
@@ -0,0 +1,18 @@
+# agora -> webdriver manager
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+# Indica o navegador que será utilizado
+servico = Service(ChromeDriverManager().install())
+navegador = webdriver.Chrome(service=servico)
+navegador.maximize_window()
+navegador.get('https://www.google.com/')
+
+# Pega a cotação do dólar
+navegador.find_element(By.XPATH, "/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input").send_keys('cotação dolar')
+navegador.find_element(By.XPATH, "/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input").send_keys(Keys.ENTER)
+
+input('')
