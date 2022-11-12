5b192200021688f8ec8f9a1c7f1a6ab75a8aafdb
jdeveloperanalyst <jonatas1200@icloud.com>
2022-11-12 17:47:35 -0300

    Pega cotação do euro

--- Automacao-de-Processos.py
+++ Automacao-de-Processos.py
@@ -5,16 +5,24 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 
+
 # Indica o navegador que será utilizado
 servico = Service(ChromeDriverManager().install())
 navegador = webdriver.Chrome(service=servico)
 navegador.maximize_window()
 navegador.get('https://www.google.com/')
 
-# Pega a cotação do dólar
+# Pega a cotação do Dólar
 navegador.find_element(By.XPATH, '/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input').send_keys('cotação dolar')
 navegador.find_element(By.XPATH, '/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input').send_keys(Keys.ENTER)
 cotacao_dolar = navegador.find_element(By.XPATH, '//*[@id="knowledge-currency__updatable-data-column"]/div[1]/div[2]/span[1]').get_attribute('data-value')
 print(f'{cotacao_dolar}, {type(cotacao_dolar)}')
+
+# Pega cotação do Euro
+navegador.get('https://www.google.com/')
+navegador.find_element(By.XPATH, '/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input').send_keys('cotação euro')
+navegador.find_element(By.XPATH, '/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input').send_keys(Keys.ENTER)
+cotacao_euro = navegador.find_element(By.XPATH, '//*[@id="knowledge-currency__updatable-data-column"]/div[1]/div[2]/span[1]').get_attribute('data-value')
+print(f'{cotacao_euro}, {type(cotacao_euro)}')
 
 input('')
