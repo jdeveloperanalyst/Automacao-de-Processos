06676d5445d1ed48bbc3dfe0de26cd16502ec016
jdeveloperanalyst <jonatas1200@icloud.com>
2022-11-12 19:23:41 -0300

    Atualizando cotação e preços de compra e venda na tabela

--- Automacao-de-Processos.py
+++ Automacao-de-Processos.py
@@ -1,9 +1,10 @@
-# agora -> webdriver manager
+# Agora -> webdriver manager
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+import pandas as pd
 
 
 # Indica o navegador que será utilizado
@@ -25,10 +26,24 @@ navegador.find_element(By.XPATH, '/html/body/div[1]/div[3]/form/div[1]/div[1]/di
 cotacao_euro = navegador.find_element(By.XPATH, '//*[@id="knowledge-currency__updatable-data-column"]/div[1]/div[2]/span[1]').get_attribute('data-value')
 print(f'{cotacao_euro}, {type(cotacao_euro)}')
 
-# Pega a cotação do Ouro,
+# Pega a cotação do Ouro
 navegador.get('https://www.melhorcambio.com/ouro-hoje')
 cotacao_ouro = navegador.find_element(By.XPATH, '//*[@id="comercial"]').get_attribute('value')
 cotacao_ouro = cotacao_ouro.replace(',', '.')
 print(f'{cotacao_ouro}, {type(cotacao_ouro)}')
+
+# Leitura da base de dados
+tabela = pd.read_excel('Produtos.xlsx')
+print(tabela)
+
+# Atualizando valor da cotação atual na tabela existente
+tabela.loc[tabela['Moeda'] == 'Dólar', 'Cotação'] = float(cotacao_dolar)
+tabela.loc[tabela['Moeda'] == 'Euro', 'Cotação'] = float(cotacao_euro)
+tabela.loc[tabela['Moeda'] == 'Ouro', 'Cotação'] = float(cotacao_ouro)
+
+# Recalculando as colunas "Preço de Compra" e "Preço de Venda" da tabela
+tabela['Preço de Compra'] = tabela['Preço Original'] * tabela['Cotação']
+tabela['Preço de Venda'] = tabela['Preço de Compra'] * tabela['Margem']
+print(tabela)
 
 input('')
